df851f3908a1ce6d255af99cc890b7d4f2071907
Wael Zantour <zantour.1999@gmail.com>
2020-02-04 15:28:06 +0100

    Chore: change the state of the checkboxs

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -9,10 +9,31 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool value) {},
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  @override
+  _TaskCheckboxState createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue;
+          
+        });
+        
+      },
     );
   }
 }
