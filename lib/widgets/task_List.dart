1f58f793068bebb07e6878a61372a8cef025b67a
Wael Zantour <zantour.1999@gmail.com>
2020-02-05 04:00:30 +0100

    Chore :  Delete Tasks by long presses

--- lib/widgets/task_List.dart
+++ lib/widgets/task_List.dart
@@ -9,18 +9,19 @@ class TasksList extends StatelessWidget {
     return Consumer<TaskData>(builder: (context, taskData, child) {
       return ListView.builder(
         itemBuilder: (context, index) {
-          final task=taskData.tasks[index];
+          final task = taskData.tasks[index];
           return TaskTile(
-              taskTitle: task.name,
-              isChecked: task.isDone,
-              checkboxCallback: (bool checkboxState) {
-               taskData.updateTask(task);
-              });
-
-
+            taskTitle: task.name,
+            isChecked: task.isDone,
+            checkboxCallback: (bool checkboxState) {
+              taskData.updateTask(task);
+            },
+            longPressCallback: (){
+              taskData.deleteTask(task);
+            },
+          );
         },
         itemCount: taskData.taskCount,
-
       );
     });
   }
